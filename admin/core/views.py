6b7baae5daf120a4d426c2164b4754274f36a41f
Shee69 <lehoangnamtep@gmail.com>
2023-06-02 15:53:24 +0700

    Remove redundancy

--- admin/core/views.py
+++ admin/core/views.py
@@ -1,59 +1,60 @@
 from django.shortcuts import render
-
+from django.core.cache import cache
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Role, Task, Admin, User
 from .serializer import RoleSerializer, TaskSerializer, AdminSerializer, UserSerializer
 
+CACHE_TTL = 60 * 15
+
 class RoleAPIView(APIView):
     def get(self, request):
-        roles = Role.objects.all()
-        serializer = RoleSerializer(roles, many=True)
-        return Response(serializer.data)
+        return cacheOrSend(Role, 'role')
 
     def post(self, request):
         serializer = RoleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        return checkValidity(serializer)
+    
 class TaskAPIView(APIView):
     def get(self, request):
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
+        return cacheOrSend(Task, 'task')
 
     def post(self, request):
         serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        return checkValidity(serializer)
+    
 class AdminAPIView(APIView):
     def get(self, request):
-        admins = Admin.objects.all()
-        serializer = AdminSerializer(admins, many=True)
-        return Response(serializer.data)
+        return cacheOrSend(Admin, 'admin')
 
     def post(self, request):
         serializer = AdminSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        return checkValidity(serializer)
+    
 class UserAPIView(APIView):
     def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
+        return cacheOrSend(User, 'user')
+        
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return checkValidity(serializer)
+
+def checkValidity(serializer):
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def cacheOrSend(model, modelName):
+    cached_data = cache.get(f"{modelName}_list")
+    if cached_data:
+        return Response(cached_data)
+    
+    users = model.objects.all()
+    serializer = RoleSerializer(users, many=True)
+    response_data = serializer.data
+    
+    cache.set(f"{modelName}_list", response_data, CACHE_TTL)
+    
+    return Response(response_data)
