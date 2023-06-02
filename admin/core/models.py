27c0dea09e0312b62aa57c44d3d43edeaabbe226
Shee69 <lehoangnamtep@gmail.com>
2023-06-02 14:38:19 +0700

    Create models

--- admin/core/models.py
+++ admin/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField
+
+class Task(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField
+
+class Admin(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    supervisor = models.ForeignKey(Admin, on_delete=models.CASCADE, null=True, blank=True)
+    
