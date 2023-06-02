abe825f8a8d91feb66d6f892f86b85eef1a6116b
Shee69 <lehoangnamtep@gmail.com>
2023-06-02 14:39:57 +0700

    Change max_length of fields

--- admin/core/models.py
+++ admin/core/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 class Role(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=30, unique=True)
     description = models.TextField
 
 class Task(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=30, unique=True)
     description = models.TextField
 
 class Admin(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    name = models.CharField(max_length=30)
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
     
 class User(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=30)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     supervisor = models.ForeignKey(Admin, on_delete=models.CASCADE, null=True, blank=True)
