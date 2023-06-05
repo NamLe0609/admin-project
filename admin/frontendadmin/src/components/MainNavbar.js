2d39f5cc0bf92b30cac325c4f911feff8acaded7
Shee69 <lehoangnamtep@gmail.com>
2023-06-05 17:10:18 +0700

    Add todo list for next time

--- admin/frontendadmin/src/components/MainNavbar.js
+++ admin/frontendadmin/src/components/MainNavbar.js
@@ -6,7 +6,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 const MainNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Container className='align-content-around'>
+      <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
--- admin/frontendadmin/src/components/mainPage.js
+++ admin/frontendadmin/src/components/mainPage.js
@@ -4,6 +4,8 @@ import { getCookie } from "../getCookie.js";
 
 import MainNavbar from "./MainNavbar.js";
 import Container from "react-bootstrap/esm/Container.js";
+import Row from "react-bootstrap/esm/Row.js";
+import Col from "react-bootstrap/esm/Col.js";
 
 const BASE_URL = "http://127.0.0.1:8000/";
 const CSRFTOKEN = getCookie("csrftoken");
@@ -14,9 +16,38 @@ const MainPage = () => {
         console.log(JSON.parse(window.sessionStorage.getItem("user")));
     }
 
+    // Todo
+
+    // Implement ways to:
+    // Add/remove employees/tasks/roles (Navbar buttons)
+    // Assign/unassign employees to tasks (Interface with dropdown of employees applicable)
+    // Make the tasks run in Celery?
+    // Add ways to mass add all tasks to main page
+    // Logout button? (not important)
+
     return (
       <Container fluid className="px-0">
-        <MainNavbar/>
+        <Row><MainNavbar/></Row>
+        <Row className="my-5">
+          <Col><div>Task</div></Col>
+          <Col><div>Content of Task</div></Col>
+          <Col><div>Assign employee to task</div></Col>
+        </Row>
+        <Row className="my-5">
+          <Col><div>Task</div></Col>
+          <Col><div>Content of Task</div></Col>
+          <Col><div>Assign employee to task</div></Col>
+        </Row>
+        <Row className="my-5">
+          <Col><div>Task</div></Col>
+          <Col><div>Content of Task</div></Col>
+          <Col><div>Assign employee to task</div></Col>
+        </Row>
+        <Row className="my-5">
+          <Col><div>Task</div></Col>
+          <Col><div>Content of Task</div></Col>
+          <Col><div>Assign employee to task</div></Col>
+        </Row>
       </Container>
     );
   };
