f909bef564aa4923d17a5431069740e85577ba40
Shee69 <lehoangnamtep@gmail.com>
2023-06-12 17:20:02 +0700

    Make new component Task for main page

---
+++ admin/frontendadmin/src/components/TaskComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+
+const TaskComponent = () => {
+  return (
+    <Row className="my-5 align-items-center">
+      <Col className="d-flex justify-content-center">
+        <h4>Task</h4>
+      </Col>
+      <Col className="d-flex justify-content-center">
+        <div>
+          Content of Task
+        </div>
+      </Col>
+      <Col className="d-flex justify-content-center">
+        <div className="assign-buttons d-flex flex-column align-items-center">
+          <Button className="btn btn-primary mb-2">Assign Employee</Button>
+          <Button className="btn btn-danger">Remove Employee</Button>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default TaskComponent;
--- admin/frontendadmin/src/components/mainPage.js
+++ admin/frontendadmin/src/components/mainPage.js
@@ -3,53 +3,35 @@ import axios from "axios";
 import { getCookie } from "../getCookie.js";
 
 import MainNavbar from "./MainNavbar.js";
-import Container from "react-bootstrap/esm/Container.js";
-import Row from "react-bootstrap/esm/Row.js";
-import Col from "react-bootstrap/esm/Col.js";
+import TaskComponent from "./TaskComponent.js";
+import { Container, Row } from "react-bootstrap";
 
 const BASE_URL = "http://127.0.0.1:8000/";
 const CSRFTOKEN = getCookie("csrftoken");
+const adminInfo = JSON.parse(window.sessionStorage.getItem("admin"));
 
 const MainPage = () => {
-
-    const getUserInfo = (e) => {
-        console.log(JSON.parse(window.sessionStorage.getItem("user")));
-    }
-
-    // Todo
-
-    // Implement ways to:
-    // Add/remove employees/tasks/roles (Navbar buttons)
-    // Assign/unassign employees to tasks (Interface with dropdown of employees applicable)
-    // Make the tasks run in Celery?
-    // Add ways to mass add all tasks to main page
-    // Logout button? (not important)
-
-    return (
-      <Container fluid className="px-0">
-        <Row><MainNavbar/></Row>
-        <Row className="my-5">
-          <Col><div>Task</div></Col>
-          <Col><div>Content of Task</div></Col>
-          <Col><div>Assign employee to task</div></Col>
-        </Row>
-        <Row className="my-5">
-          <Col><div>Task</div></Col>
-          <Col><div>Content of Task</div></Col>
-          <Col><div>Assign employee to task</div></Col>
-        </Row>
-        <Row className="my-5">
-          <Col><div>Task</div></Col>
-          <Col><div>Content of Task</div></Col>
-          <Col><div>Assign employee to task</div></Col>
-        </Row>
-        <Row className="my-5">
-          <Col><div>Task</div></Col>
-          <Col><div>Content of Task</div></Col>
-          <Col><div>Assign employee to task</div></Col>
-        </Row>
-      </Container>
-    );
+  const getUserInfo = (e) => {
+    console.log(JSON.parse(window.sessionStorage.getItem("user")));
   };
-  
-  export default MainPage;
+
+  // Todo
+
+  // Implement ways to:
+  // Assign/unassign employees to tasks (Interface with dropdown of employees applicable)
+  // Make the tasks run in Celery?
+  // Add ways to mass add all tasks to main page
+  // Logout button? (not important)
+
+  return (
+    <Container fluid className="px-0">
+      <Row>
+        <MainNavbar />
+      </Row>
+      <TaskComponent/>
+      <TaskComponent/>
+    </Container>
+  );
+};
+
+export default MainPage;
